e38661695efa8359c6872dab9a7a3bfee158b3d7
Amish Mathur <87874161+Amishmathur1@users.noreply.github.com>
2026-07-01 13:52:32 +0530

    Add: combination-target-sum-ii - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum2(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        ans = []
+        nums.sort()
+        visited = set()
+
+        def dfs(start, path, summ):
+            if summ == target:
+                ans.append(path[:])
+                return 
+
+            if summ > target or summ < 0:
+                return 
+                
+
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                new_sum = summ + nums[i]
+                dfs(i+1, path, new_sum)
+
+                path.pop()
+                new_sum = summ - nums[i]
+        dfs(0, [], 0)
+        return ans
